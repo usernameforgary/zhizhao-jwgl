b5cd5397f6b90ce6f93aafa778c6f51d8f2527c2
Gary Chen <usernameforgary@gmail.com>
2021-08-28 09:16:05 +0800

    feat: temp save

--- src/main/java/com/zhizhao/jwgl/jiaowuguanli/config/security/MySecurityConfig.java
+++ src/main/java/com/zhizhao/jwgl/jiaowuguanli/config/security/MySecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
